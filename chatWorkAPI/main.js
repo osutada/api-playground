3d278a60c4a97c9ebb0681eba73dcb3c2857d3ae
osutada <develop@osutada.jp>
2024-12-21 23:22:16 +0900

    chatWorkメッセージ送信API、Discodeメッセージ送信API作成

---
+++ chatWorkAPI/main.js
@@ -0,0 +1,36 @@
+/**
+ * ChatWork APIを使ってメッセージを投稿する関数
+ * https://developer.chatwork.com/docs/endpoints
+ *
+ * @param {string} message - 投稿するメッセージ。指定されていない場合はエラーログを出力する。
+ */
+const sendChatWorkPost = (message) => {
+  // メッセージがない場合は処理を中断
+  if (!message) {
+    Logger.log("ERROR: not found message");
+    return;
+  }
+
+  // CHATWORKのAPIトークン
+  const CHATWORK_API_TOKEN = "***";
+  // CHATWORKのルームID
+  const CHATWORK_ROOM_ID = "***";
+  // エンドポイント
+  const MESSAGE_POST_ENDPOINT = `https://api.chatwork.com/v2/rooms/${CHATWORK_ROOM_ID}/messages`;
+
+  // HTTPリクエストのオプション
+  const options = {
+    method: "post",
+    headers: {
+      "X-ChatWorkToken": CHATWORK_API_TOKEN, // ヘッダーにAPIトークンをセット
+    },
+    payload: {
+      body: message, // メッセージをペイロードにセット
+    },
+  };
+
+  // APIリクエストを実行
+  const response = UrlFetchApp.fetch(MESSAGE_POST_ENDPOINT, options);
+  // 結果をログに表示
+  Logger.log(response.getContentText());
+};
---
+++ discodeAPI/main.js
@@ -0,0 +1,29 @@
+/**
+ * DiscodAPIを使ってメッセージを投稿する関数
+ *
+ * @param {string} message - 投稿するメッセージ。指定されていない場合はエラーログを出力する。
+ */
+const sendDiscodePost = (message) => {
+  // メッセージがない場合は処理を中断
+  if (!message) {
+    Logger.log("ERROR: not found message");
+    return;
+  }
+
+  // DISCODEのWebhook URL
+  const DISCODE_WEBHOOK_URL = "***";
+
+  // HTTPリクエストのオプション
+  const options = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify({
+      content: message, // メッセージをペイロードにセット
+    }),
+  };
+
+  // APIリクエストを実行
+  const response = UrlFetchApp.fetch(DISCODE_WEBHOOK_URL, options);
+  // 結果をログに表示
+  Logger.log(response.getContentText());
+};
